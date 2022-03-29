a743a9046945a00b2ad1b73a8fe796c255ed8a84
susha12 <87581584+susha12@users.noreply.github.com>
2022-03-29 15:18:18 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let inputOne = document.querySelector(".card_number");
+let inputTwo = document.querySelector(".cvv");
+
+inputOne.onclick = ()=>{
+    inputOne.value = "";
+}
+inputTwo.onclick = ()=>{
+    inputTwo.value = "";
+}
