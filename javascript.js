65200295be07a721cfac0770aebdd2607f3de4d2
susha12 <87581584+susha12@users.noreply.github.com>
2022-04-03 17:39:40 +0400

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-let inputOne = document.querySelector(".card_number");
-let inputTwo = document.querySelector(".cvv");
-
-inputOne.onclick = ()=>{
-    inputOne.value = "";
-}
-inputTwo.onclick = ()=>{
-    inputTwo.value = "";
-}
